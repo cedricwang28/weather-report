dc114e176a72b74389ea0d900389d553556d881d
Cedric <cedricwang28@gmail.com>
2024-03-14 15:40:46 -0600

    first commit

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Calgary",
+    country: "CA",
+    state: "Alberta",
+    lat: 51.049999,
+    lon: -114.066666,
+  },
+  {
+    name: "Edmonton",
+    country: "CA",
+    state: "Alberta",
+    lat: 53.5444,
+    lon: -113.4909,
+  },
+  {
+    name: "Vancouver",
+    country: "CA",
+    state: "BC",
+    lat: 49.24966,
+    lon: -123.11934,
+  },
+  {
+    name: "Toronto",
+    country: "CA",
+    state: "Ontario",
+    lat: 43.6532,
+    lon: -79.3832,
+  },
+  {
+    name: "Montreal",
+    country: "CA",
+    state: "Quebec",
+    lat: 45.509,
+    lon: -73.562,
+  },
+];
+
+export default defaultStates;
