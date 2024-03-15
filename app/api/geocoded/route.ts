2a7eb33a61ee21ae37d458907229adf203832a61
Cedric <cedricwang28@gmail.com>
2024-03-15 02:17:48 -0600

    fix error

--- app/api/geocoded/route.ts
+++ app/api/geocoded/route.ts
@@ -1,7 +1,5 @@
-"use server"
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
-import { isDynamicServerError } from "next/dist/client/components/hooks-server-context";
 
 export async function GET(req: NextRequest) {
   const apiKey = process.env.OPENWEATHERMAP_API_KEY;
@@ -15,9 +13,6 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(res.data);
   } catch (error) {
     console.log("Error fetching geocoded data");
-    if (isDynamicServerError(error)) {
-      throw error;
-    }
     return new Response("Error fetching geocoded data", { status: 500 });
   }
 }
